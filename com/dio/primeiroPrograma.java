af3ef209ba0e8b9df68f13129f962e7a1987c5a0
gentile95 <lucas.gentile95@gmail.com>
2023-03-13 22:02:07 -0300

    uploading my first java programs

---
+++ com/dio/primeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.gato.Gato;
+
+public class primeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+
+	}
+	
+}
+
